6fd55c4e045d820028301dc84a75055b969488f0
Jota Jota <jasmyxelf@gmail.com>
2013-06-20 19:09:18 -0400

    BASH: Define the 'Handy Colors' in '!base.system.bash' and implement 'hcolors.plugins.bash'

---
+++ bash/plugins/hcolors.plugins.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#
+# Function: hcolors()
+#
+# Prints out the 'Handy Colors'
+#
+hcolors () {
+  echo -e "${BLACK}\${BLACK}"
+  echo -e "${BLUE}\${BLUE}         ${LIGHT_BLUE}\${LIGHT_BLUE}"
+  echo -e "${GREEN}\${GREEN}       ${LIGHT_GREEN}\${LIGHT_GREEN}"
+  echo -e "${CYAN}\${CYAN}         ${LIGHT_CYAN}\${LIGHT_CYAN}"
+  echo -e "${RED}\${RED}           ${LIGHT_RED}\${LIGHT_RED}"
+  echo -e "${PURPLE}\${PURPLE}     ${LIGHT_PURPLE}\${LIGHT_PURPLE}"
+  echo -e "${BROWN}\${BROWN}            ${YELLOW}\${YELLOW}"
+  echo -e "${GRAY}\${GRAY}         ${LIGHT_GRAY}\${LIGHT_GRAY}"
+  echo -e "${WHITE}\${WHITE}\n"
+  echo -e "${NCOLOR}\${NCOLOR} (No Color)"
+}
+
