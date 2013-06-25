004d1fc8f25c6511b83b049319c454b61ed77e9b
Jota Jota <jasmyxelf@gmail.com>
2013-06-24 23:15:44 -0400

    BASH: Revamp the 'Handy Colors' to extend its functionality. Define it into '_hcolors_definition()' in '!base.system.bash'

--- bash/system/!base.system.bash
+++ bash/system/!base.system.bash
@@ -8,24 +8,69 @@
 export E_SUCCESS=0
 export E_FAILURE=1
 
-# Handy Colors. See 'hcolors.plugins.bash'
-export BLACK='\033[0;30m'
-export BLUE='\033[0;34m'
-export GREEN='\033[0;32m'
-export CYAN='\033[0;36m'
-export RED='\033[0;31m'
-export PURPLE='\033[0;35m'
-export BROWN='\033[0;33m'
-export GRAY='\033[1;30m'
-export LIGHT_GRAY='\033[0;37m'
-export LIGHT_BLUE='\033[1;34m'
-export LIGHT_GREEN='\033[1;32m'
-export LIGHT_CYAN='\033[1;36m'
-export LIGHT_RED='\033[1;31m'
-export LIGHT_PURPLE='\033[1;35m'
-export YELLOW='\033[1;33m'
-export WHITE='\033[1;37m'
-export NCOLOR='\033[0m'
+#
+# Function: _hcolors_definition()
+#
+# Handy Colors definitions. Declared in a function because of the mechanism
+# used to demo the definitions in hcolors(). Also to unclutter this file.
+#
+# See 'hcolors.plugins.bash'
+#
+_hcolors_definition () {
+  # Foreground :: Normal
+  export BLACK='\033[0;30m'
+  export RED='\033[0;31m'
+  export GREEN='\033[0;32m'
+  export YELLOW='\033[0;33m'
+  export BLUE='\033[0;34m'
+  export MAGENTA='\033[0;35m'
+  export CYAN='\033[0;36m'
+  export GRAY='\033[0;37m'
+
+  # Foreground :: Intense
+  export I_GRAY='\033[1;30m'
+  export I_RED='\033[1;31m'
+  export I_GREEN='\033[1;32m'
+  export I_YELLOW='\033[1;33m'
+  export I_BLUE='\033[1;34m'
+  export I_MAGENTA='\033[1;35m'
+  export I_CYAN='\033[1;36m'
+  export WHITE='\033[1;37m'
+
+  # Foreground :: Underline
+  export U_BLACK='\033[4;30m'
+  export U_RED='\033[4;31m'
+  export U_GREEN='\033[4;32m'
+  export U_YELLOW='\033[4;33m'
+  export U_BLUE='\033[4;34m'
+  export U_MAGENTA='\033[4;35m'
+  export U_CYAN='\033[4;36m'
+  export U_GRAY='\033[4;37m'
+
+  # Background :: Normal
+  export ON_BLACK='\033[40m'
+  export ON_RED='\033[41m'
+  export ON_GREEN='\033[42m'
+  export ON_YELLOW='\033[43m'
+  export ON_BLUE='\033[44m'
+  export ON_MAGENTA='\033[45m'
+  export ON_CYAN='\033[46m'
+  export ON_WHITE='\033[47m'
+
+  # Background :: Intense
+  export ON_I_BLACK='\033[100m'
+  export ON_I_RED='\033[101m'
+  export ON_I_GREEN='\033[102m'
+  export ON_I_YELLOW='\033[103m'
+  export ON_I_BLUE='\033[104m'
+  export ON_I_MAGENTA='\033[105m'
+  export ON_I_CYAN='\033[106m'
+  export ON_I_WHITE='\033[107m'
+
+  # Reset
+  export R_COLOR='\033[0m'
+} &&
+_hcolors_definition
 
 # --------------------------------------------------------------------------------------------------
 #  Base Functions
