49ab261bc24c219e078816b3a8056b661557eb5a
Jota Jota <jasmyxelf@gmail.com>
2013-07-01 21:22:02 -0400

    BASH: Create completion for Git commands and aliases in 'git.completions.bash'

---
+++ bash/completions/git.completions.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# g => git
+if [ -r /usr/share/bash-completion/completions/git ]; then
+  source /usr/share/bash-completion/completions/git
+  complete -o bashdefault -o default -o nospace -F _git g
+fi
+
