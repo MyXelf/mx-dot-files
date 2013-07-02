96bb75ff5d57abf8b156923581782f36bdc24276
Jota Jota <jasmyxelf@gmail.com>
2013-07-01 20:46:20 -0400

    BASH: Define basic aliases for 'Directories Listings, Navigation, Handling' and 'Verbose File Operations' in 'base.aliases.bash'

---
+++ bash/aliases/base.aliases.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Directories Listings
+alias ls='ls --color=auto'
+alias ll='ls --all --human-readable --classify -l'
+alias l1='ls --almost-all -1'
+alias l?='ll | ?'
+alias lw='l1 | wcl'
+alias l='ll'
+
+# Directories Navigation & Handling
+alias ..='cd ..'
+alias ..2='cd ../..'
+alias ..3='cd ../../..'
+alias ..4='cd ../../../..'
+alias ..5='cd ../../../../..'
+alias ..?='path_leader'
+alias -- -='cd -'
+
+mkcd () { mkdir -p "$@" && cd "$@"; }
+
+# Verbose File Operations
+alias cp='cp -v'
+alias mv='mv -v'
+alias rm='rm -v'
+
