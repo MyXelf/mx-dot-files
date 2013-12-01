1177c7dcf07fa3673c48f77eaa495b1c39a02086
Jota Jota <jasmyxelf@gmail.com>
2013-12-01 18:59:34 -0500

    BASH: Guarantee the completion for Drush command and aliases in 'drush.completions.bash'

---
+++ bash/completions/drush.completions.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# d => drush
+if [ -r /usr/share/bash-completion/completions/drush ]; then
+  source /usr/share/bash-completion/completions/drush
+  # There is no need to complete the 'd' alias, because it is already included
+  # in the default 'drush.complete.sh' script.
+fi
