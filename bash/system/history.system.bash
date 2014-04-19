4d9a127c656b1d157c778a1308d4b4cb8057fedb
Jota Jota <jasmyxelf@gmail.com>
2014-04-18 21:50:18 -0400

    BASH: Reallocate the 'Out-Of-Service Mode' (OOSM) from '!base.system.bash' to 'history.system.bash'
    Include a proper statement about what to do with the OOSM_AUTOSTART
    environment variable to enable or disable the functionality.

--- bash/system/history.system.bash
+++ bash/system/history.system.bash
@@ -1,7 +1,60 @@
+# --------------------------------------------------------------------------------------------------
+#  OOSM :: Out-Of-Service Mode
+# --------------------------------------------------------------------------------------------------
+
+# The Out-Of-Service Mode will avoid the typed commands to be stored on the
+# History. The main purpose is to keep your History clear of trivial commands
+# (e.g. long testing sessions). Of course can also be used for illegal
+# activities you don't want to be tracked down ;-)
+#
+# To enable this functionality by default at startup, export the following
+# at someplace before this file (i.e. !bash_profile.local.bash).
+#
+# export OOSM_AUTOSTART=1
+
+#
+# Function: oos() = 'Out-Of-Service'
+#
+# Enable the OOSM
+#
+oos () {
+  if [ -n "$HISTFILE" ]; then
+    export OOSM_HFILE=$HISTFILE
+    unset HISTFILE
+  fi
+}
+
+#
+# Function: bos() = 'Back-On-Service'
+#
+# Disable the OOSM
+#
+bos () {
+  if [ -n "$OOSM_HFILE" ]; then
+    export HISTFILE=$OOSM_HFILE
+    unset OOSM_HFILE
+    history -c
+    history -r
+  fi
+}
+
+#
+# Function: oosm_active()
+#
+# Determine if the Out-Of-Service Mode is active
+#
+oosm_active () {
+  [ -z "$HISTFILE" ]
+}
+
 # Determine if the OOSM must be reactivated
 if [ -n "$OOSM_AUTOSTART" ] || oosm_active; then
   oosm_reactivate=1
 fi
+
+# --------------------------------------------------------------------------------------------------
+#  History Options
+# --------------------------------------------------------------------------------------------------
 
 # History filename
 export HISTFILE=$MXDF_BASH_LOCAL/bash_history
@@ -15,11 +68,9 @@ export HISTSIZE=10000
 # Ignore duplicate and commands starting with spaces
 export HISTCONTROL=ignorespace:ignoredups
 
-#
 # Ignore commands following this pattern. Beware of the History substitution
 # commands (i.e. '!!'), as you may think you are repeating something that is
 # not saved in the history.
-#
 export HISTIGNORE="&"                                      # Repeated commands
 export HISTIGNORE=${HISTIGNORE}":~:-:..:..[2345?]:cd *"    # Directories Navigation
 export HISTIGNORE=${HISTIGNORE}":l:l[slw1]:l\? *"          # Directories Listings
